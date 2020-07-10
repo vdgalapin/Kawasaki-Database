ba1bac76f31f107a245cf4c8d63a486ebddff429
Vyron Galapin <59787986+vdgalapin@users.noreply.github.com>
2020-07-10 14:52:57 -0500

    Add Pie Chart
    I added a piechart.

--- Efficiency.php
+++ Efficiency.php
@@ -36,32 +36,51 @@
 	
 	# Show the results in a table
 	echo 
-	"<table border='1'>
-		<tr>
-			<td><b>First Name</b></td>
-			<td><b>Last Name</b></td>
-			<td><b>Employee ID Number</b></td>
-			<td><b>Average</b></td>
-		</tr>
-		<tr>
-			<td>".$row['First']."</td>
-			<td>".$row['Last']."</td>
-			<td>".$empId."</td>
-			<td>".$row['Average']."</td>
-		</tr>
-	</table>";
+	"<h1 style='color:red; margin-bottom:-25px; font-family: Impact, Charcoal, sans-serif;'>".$row['First']." ".$row['Last']."</h1>
+	<h2  style='color:red; margin-bottom:-25px; font-family: Impact, Charcoal, sans-serif;'>".$empId."</h2>";
+
 	
 	# Show the results of information
-	echo "</br>Performance Percentage </br><progress style = 'border: none; width: 400px; height: 60px; background: crimson;' value='".$row['Average']."' max = '100'></progress>";
+	# This will be the piechart
+	# I subtract the inefficiency number here because it wont let me subtract in the echo.
+	$inefficiency = 100 - $row['Average'];
+		echo "	<div id='piechart' style='position:absolute; top:2%; right:50%; border: 5px solid red; border-radius:10px;'></div>
+			<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
+			<script type='text/javascript'>
+			// Load google charts
+			google.charts.load('current', {'packages':['corechart']});
+			google.charts.setOnLoadCallback(drawChart);
+
+			// Draw the chart and set the chart values
+			function drawChart() {
+			  var data = google.visualization.arrayToDataTable([
+			  ['', ''],
+			  ['Working Efficieny(%)',".$row['Average']."],
+			  ['Inefficient(%)', ".$inefficiency."],
+			]);
+
+			  // Optional; add a title and set the width and height of the chart
+			  var options = {'title':'Performance', 'width':200, 'height':200, 'legend':'none'};
+
+			  // Display the chart inside the <div> element with id='piechart'
+			  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+			  chart.draw(data, options);
+			}
+			</script>
+			";
+
 	$query = "SELECT DISTINCT(DepartmentLineNumber) FROM RUNNING WHERE empId1 =".$empId." OR empId2 = ".$empId.";";
 	$results = mysqli_query($connection, $query);
 	while ($row = mysqli_fetch_array($results)) {
-		echo "<br>".$row['DepartmentLineNumber'].": ";
+		echo "<br>".$row['DepartmentLineNumber']." ";
 		$query = "SELECT AVG(Efficiency) as Average FROM RUNNING WHERE (empId1 =".$empId." OR empId2 =".$empId.") AND DepartmentLineNumber = '".$row['DepartmentLineNumber']."'";
 		$res = mysqli_query($connection, $query);
 		$r = mysqli_fetch_array($res);
 		echo $r['Average'];
 		echo "<progress style = 'border: none; background: crimson;' value='".$r['Average']."' max = '100'></progress>";
-	}	?>
+		echo"<br><br>";
+	}	
+
+	?>
 </body>
 </html>
